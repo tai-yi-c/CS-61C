ecbe41862e40e6d1a3678ba0286e82a2e6461180
tai-yi-c <2359625501@qq.com>
2022-06-05 21:50:04 +0800

    finish all lab01

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -3,4 +3,23 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+	if(!head) {
+		return 0;
+	}
+	node *slow_ptr = head;
+	node *fast_ptr = head -> next;
+	while(fast_ptr && fast_ptr != slow_ptr) {
+		slow_ptr = slow_ptr -> next;
+		fast_ptr = fast_ptr -> next;
+		if(fast_ptr) {
+			fast_ptr = fast_ptr -> next;
+		}else {
+			break;
+		}
+	}
+	if(!fast_ptr) {
+		return 0;
+	}else {
+		return 1;
+	}
 }
